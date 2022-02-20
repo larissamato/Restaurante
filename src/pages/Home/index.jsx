c537036e6132be857a8a7d29734f99ae28a1b99f
larissamato <matoslarissa31@gmail.com>
2022-02-20 13:34:53 -0300

    lateral

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import { Container } from './styles';
+import React, {useState} from 'react';
+import TextField, {Input} from '@material/react-text-field';
+import logo from '../../assets/logo.svg';
+import { Container, Search} from './styles';
 
-const Home =()=>( 
-<Container>Hello World!!</Container>
-);
+const Home =()=>{
+const [inputValue, setInputValue]= useState('');
+    return(
+    <Container>
+    <Search>
+        <img src={logo} alt="logo"/>
+        <TextField label='Pesquisar' outlined>
+            <Input value={inputValue} onChange={(e)=>setInputValue(e.target.value)}>
+
+            </Input>
+        </TextField>
+        </Search> 
+    </Container>
+    );
+};
 
 export default Home;
