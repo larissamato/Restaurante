ce690cc22c535f07fc92c5b020e4ed00fdfca0a0
larissamato <matoslarissa31@gmail.com>
2022-02-20 09:40:34 -0300

    parte1

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+//TEMA GERAL, FÁCIL DE SER MUDADO 
+
+const theme ={
+colors:{
+    primary: '#6200ee',
+    background:'#00000014',
+    text: '#000000B3',
+    success: '#135e01',
+},
+fonts:{
+    regular: 'Roboto, sans-serif',
+},
+};
+export default theme;
